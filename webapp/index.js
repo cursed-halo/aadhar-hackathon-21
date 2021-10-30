973f0800a8b3eb7434eb43b224f2698ba11956c5
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2021-10-30 23:57:13 +0530

    changes in login

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"></script>
-  </body>
+  <body>
   <script src='https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js'></script>
   <script type="module">
      import fs from 'fs';
