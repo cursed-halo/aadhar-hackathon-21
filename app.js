ae8d97cb9cbe3467c141b7a9b784df52d86517ba
Kaiba <62166257+cursed-halo@users.noreply.github.com>
2021-10-31 22:41:28 +0530

    tenant_add_edit.html, app.js, idcard.html changed

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs=require('fs');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('tenant_add_edit.html',function(error,data)
+  {
+      if(error)
+      {
+          res.writeHead(404);
+          res.write("Error");
+      }
+      else{
+          res.write(data)
+      }
+      res.end();
+  })
+  
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
