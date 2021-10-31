1953ccd836f76fc5aa863dbb394fac68570e2e6c
Kaiba <62166257+cursed-halo@users.noreply.github.com>
2021-10-31 20:59:03 +0530

    replaced old files with fixed ones for landlord and tenant sign in

--- webapp/app.js
+++ webapp/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  fs.readFile('sign_tenant.html',function(error,data)
+  fs.readFile('sign_landlord.html',function(error,data)
   {
       if(error)
       {
